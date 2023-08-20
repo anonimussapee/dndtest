a6055c3201988db475f3d6d99abb661b90089abe
jdx <librededrogas420@gmail.com>
2023-08-19 22:06:21 -0500

    i added ondraggedend function

--- src/App.jsx
+++ src/App.jsx
@@ -20,31 +20,105 @@ const initialData = {
   //facilitate reordering of the columns
   columnOrder :['column-1']
 }
-class App extends React.Component{
-
-  state = initialData  
-
-  onDragEnd = (result) => {
-    //TODO: reorder our column
-
+const result = {
+  draggableId: 'task-1',
+  type:'TYPE',
+  reason: 'DROP',
+  source: {
+    droppableId:'column-1',
+    index:1
+  },
+  destination:{
+    droppableId: 'column-1',
+    index:1
   }
-  render () {
+}
+
+const App = () => {
+
+  const [data,setData] = useState(initialData)
+
+  const onDragEnd = (result) => {
+    //TODO: reorder our column
+    const {destination, source, draggableId} = result
+    
+    if(!destination){
+      return;
+    }
+
+    if( destination.droppableId === source.droppableId && destination.index === source.index){
+      return;
+    }
+
+    const column = data.columns[source.droppableId]
+    const newTaskIds = Array.from(column.taskIds)
+    newTaskIds.splice(source.index, 1)
+    newTaskIds.splice(destination.index, 0, draggableId)
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds
+    }
+
+    const newState = {
+      ...data,
+      columns:{
+      ...data.columns,
+      [newColumn.id] : newColumn
+      }
+    }
+    setData(newState)
+  }
 
     return (
-      <DragDropContext
-        onDragEnd={this.onDragEnd}
-      >
-        {
-        this.state.columnOrder?.map((columnId) =>{
-          const column = this.state.columns[columnId]
-          const tasks = column.taskIds.map(taskId => this.state.tasks[taskId])
-          return <Column key={column.id} column={column} tasks={tasks}/>
-        })
-        }
-  
-      </DragDropContext>
+      <div>
+        <DragDropContext
+          onDragEnd={onDragEnd}
+        >
+          {
+          data.columnOrder?.map((columnId) =>{
+            const column = data.columns[columnId]
+            const tasks = column.taskIds.map(taskId => data.tasks[taskId])
+            return <Column key={column.id} column={column} tasks={tasks}/>
+          })
+          }
+    
+        </DragDropContext>
+        <p >{data.columns['column-1'].taskIds}</p>
+        <p >{Object.keys(data.tasks)}</p>
+
+      </div>
     )
-  }
+  
 } 
 
 export default App
+
+// class App extends React.Component{
+
+//   state = initialData  
+
+//   onDragEnd = (result) => {
+//     //TODO: reorder our column
+
+//   }
+//   render () {
+
+//     return (
+//       <DragDropContext
+//         onDragEnd={this.onDragEnd}
+//       >
+//         {
+//         this.state.columnOrder?.map((columnId) =>{
+//           const column = this.state.columns[columnId]
+//           const tasks = column.taskIds.map(taskId => this.state.tasks[taskId])
+//           return <Column key={column.id} column={column} tasks={tasks}/>
+//         })
+//         }
+  
+//       </DragDropContext>
+//     )
+//   }
+// } 
+
+// export default App
